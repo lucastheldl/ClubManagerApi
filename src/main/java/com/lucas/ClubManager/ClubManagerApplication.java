26056f9794846727f9b3eea93d3c50bdbfa832f4
lucastheldl <lucastheldl@gmail.com>
2024-03-18 18:06:58 -0300

    first commit

---
+++ src/main/java/com/lucas/ClubManager/ClubManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.ClubManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubManagerApplication.class, args);
+	}
+
+}
